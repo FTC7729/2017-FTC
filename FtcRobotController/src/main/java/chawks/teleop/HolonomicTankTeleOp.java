0ace82256a0fe05dfd236231f12516b402baeab6
Kara Handy <karahandy@Karas-MacBook.home>
2017-10-21 19:53:19 -0400

    Start of code for 'hotkeys' trurning left and right with bumpers:

--- FtcRobotController/src/main/java/chawks/teleop/HolonomicTankTeleOp.java
+++ FtcRobotController/src/main/java/chawks/teleop/HolonomicTankTeleOp.java
@@ -1,10 +1,10 @@
 package chawks.teleop;
 
-        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-        import com.qualcomm.robotcore.hardware.Gamepad;
-        import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.Range;
 
-        import chawks.hardware.DrivingDirection;
+import chawks.hardware.DrivingDirection;
 
 @TeleOp(name = "Holonomic Tank", group = "TeleOp")
 public class HolonomicTankTeleOp extends BoxyTheTank {
@@ -99,4 +99,25 @@ public class HolonomicTankTeleOp extends BoxyTheTank {
         robot.RFMotor.setPower(rf_pow);
         robot.LFMotor.setPower(lf_pow);
     }
+
+    @Override
+    public void handleGamePad2(Gamepad gamepad){
+        //for testing purposes it is on GamePad2
+        if (gamepad.left_bumper){
+            //turns robot left by putting left side negative and right side positive
+            robot.LFMotor.setPower(-1.00);
+            robot.LBMotor.setPower(-1.00);
+            robot.RFMotor.setPower(1.00);
+            robot.RBMotor.setPower(1.00);
+
+        } else if (gamepad.right_bumper){
+            //turns robot right by putting left side positive and right side negative
+            robot.LFMotor.setPower(1.00);
+            robot.LBMotor.setPower(1.00);
+            robot.RFMotor.setPower(-1.00);
+            robot.RBMotor.setPower(-1.00);
+
+        }
+    }
+
 }
---
+++ FtcRobotController/src/main/java/chawks/teleop/HotKeys.java
@@ -0,0 +1,9 @@
+package chawks.teleop;
+
+/**
+ * Created by karahandy on 10/21/17.
+ */
+
+public class HotKeys {
+
+}
