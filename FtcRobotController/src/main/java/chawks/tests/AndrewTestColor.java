1f5dc97f33dbe764ab5c50f71089f4ed6bce34f1
123andrew12 <liandrew2003@gmail.com>
2018-01-09 19:11:09 -0500

    worked on vuforia stuffs

---
+++ FtcRobotController/src/main/java/chawks/tests/AndrewTestColor.java
@@ -0,0 +1,70 @@
+package chawks.tests;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+@TeleOp(name = "Sensor: MR Color", group = "Sensor")
+public class AndrewTestColor extends LinearOpMode {
+
+    ColorSensor colorSensor;
+    @Override
+    public void runOpMode() {
+        float hsvValues[] = {0F,0F,0F};
+
+        final float values[] = hsvValues;
+
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+        boolean bPrevState = false;
+        boolean bCurrState = false;
+
+        boolean bLedOn = true;
+
+        colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color");
+
+        colorSensor.enableLed(bLedOn);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            bCurrState = gamepad1.x;
+
+            if (bCurrState && (bCurrState != bPrevState))  {
+
+                bLedOn = !bLedOn;
+                colorSensor.enableLed(bLedOn);
+            }
+
+            bPrevState = bCurrState;
+
+            Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
+
+
+            telemetry.addData("LED", bLedOn ? "On" : "Off");
+            telemetry.addData("Clear", colorSensor.alpha());
+            telemetry.addData("Red  ", colorSensor.red());
+            telemetry.addData("Green", colorSensor.green());
+            telemetry.addData("Blue ", colorSensor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                }
+            });
+
+            telemetry.update();
+        }
+
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.WHITE);
+            }
+        });
+    }
+}
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorMRColor.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorMRColor.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@TeleOp(name = "Sensor: MR Color", group = "Sensor")
+@TeleOp(name = "Sensor:MRColor", group = "Sensor")
 //@Disabled
 public class SensorMRColor extends LinearOpMode {
 
