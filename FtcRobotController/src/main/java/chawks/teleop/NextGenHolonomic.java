c871692db9cbe1a6c3efe003bf5e97d6ee707d59
Cassie - SW Mentor <cassie@cheerfrog.com>
2018-01-11 20:12:09 -0500

    [COMPLETE] Created Holonomic and Tank mode for next gen
    all files are ready for next gen to drive in both modes tank and
    holonomic. Also, Danielle added dpad up and down for elevate motor in
    gamepad2

---
+++ FtcRobotController/src/main/java/chawks/teleop/NextGenHolonomic.java
@@ -0,0 +1,33 @@
+package chawks.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@TeleOp(name = "NextGenHolonomic ", group = "TeleOp")
+public class NextGenHolonomic extends NextGenthetank {
+
+//Hello, How Are You
+
+    public void handleGamePad1(Gamepad gamepad) {
+        double r = Math.hypot(gamepad.left_stick_x, gamepad.left_stick_y);
+        double robotAngle = Math.atan2(gamepad.left_stick_y, gamepad.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) - rightX;
+        final double v2 = r * Math.sin(robotAngle) + rightX;
+        final double v3 = r * Math.sin(robotAngle) - rightX;
+        final double v4 = r * Math.cos(robotAngle) + rightX;
+
+
+        robot.LFMotor.setPower((-v3)/2);
+        robot.RFMotor.setPower((-v4)/2);
+        robot.LBMotor.setPower((-v1)/2);
+        robot.RBMotor.setPower((-v2)/2);
+    }
+
+    public void handleGamePad2(Gamepad gamepad) {
+    }
+
+
+//LFM, RFM, LBM, RBM
+
+}
