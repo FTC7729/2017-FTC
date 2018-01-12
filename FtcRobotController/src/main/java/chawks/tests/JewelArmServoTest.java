fa0a561b78a2a7f389463e82fca0d8e594314b3a
geekkid1 <ibtronfan@gmail.com>
2018-01-11 20:05:37 -0500

    Jewel arm testing

---
+++ FtcRobotController/src/main/java/chawks/tests/JewelArmServoTest.java
@@ -0,0 +1,20 @@
+package chawks.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by ibtro on 1/11/2018.
+ */
+
+@Autonomous(name="Jewel Arm Test",group="Tests")
+public class JewelArmServoTest extends LinearOpMode {
+    Servo pivot;
+    double PIVOT_START = 0.40;
+    double ARM_DROP = 0.15;
+   // Servo
+    public void runOpMode() {
+
+    }
+}
