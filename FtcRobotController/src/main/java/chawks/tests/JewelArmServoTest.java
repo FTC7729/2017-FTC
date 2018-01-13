40869c4a3180a7d8259f72b09d8c214c9cc60f65
geekkid1 <ibtronfan@gmail.com>
2018-01-13 13:27:20 -0500

    Add functionality to Jewel Arm mechanical test.

--- FtcRobotController/src/main/java/chawks/tests/JewelArmServoTest.java
+++ FtcRobotController/src/main/java/chawks/tests/JewelArmServoTest.java
@@ -11,10 +11,29 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Autonomous(name="Jewel Arm Test",group="Tests")
 public class JewelArmServoTest extends LinearOpMode {
     Servo pivot;
-    double PIVOT_START = 0.40;
-    double ARM_DROP = 0.15;
-   // Servo
+    Servo arm;
+    double INCREMENT = 0.009;
+    double PIVOT_START = 0.39;
+    double ARM_DROP = 0.05;
+    double ARM_START = 1.0;
+    double arm_pos;
+
     public void runOpMode() {
+        //connects to actual servos
+        pivot = hardwareMap.get(Servo.class, "pivot");
+        arm = hardwareMap.get(Servo.class, "arm");
+        waitForStart();
+        //put the arm down
+        pivot.setPosition(PIVOT_START);
+        arm_pos = ARM_START;
+        while (arm_pos > ARM_DROP) {
+            if(!opModeIsActive()){ break; }
+            arm_pos -= INCREMENT;
+            arm.setPosition(arm_pos);
+            sleep(45);
+            idle();
+        }
 
     }
+    
 }
