b1be1290e6e8047e8af7a06bbb4d5b93082f072d
123andrew12 <liandrew2003@gmail.com>
2018-02-06 19:53:23 -0500

    Made the Vuforia stuff work

--- FtcRobotController/src/main/java/chawks/Autonomous/BlueAllianceStone1Vuforia.java
+++ FtcRobotController/src/main/java/chawks/Autonomous/BlueAllianceStone1Vuforia.java
@@ -163,7 +163,7 @@ public class BlueAllianceStone1Vuforia extends LinearOpMode {
                 closeLift();
                 moveLiftAmount(1,500);
                 navxTurn(90);
-                encoderDrive(.5,-5.5,-7.5,-5.5,-5.5,4);
+                encoderDrive(.5,-5.25,-5.25,-5.25,-5.25,4);
                 sleep(1000);
                 navxTurn(179);
                 encoderDrive(.5,-1,-1,-1,-1,4);
@@ -179,7 +179,7 @@ public class BlueAllianceStone1Vuforia extends LinearOpMode {
                 closeLift();
                 moveLiftAmount(1,500);
                 navxTurn(90);
-                encoderDrive(.5,-5.5,-7.5,-5.5,-5.5,4);
+                encoderDrive(.5,-6,-6,-6,-6,4);
                 sleep(1000);
                 navxTurn(179);
                 encoderDrive(.5,-1,-1,-1,-1,4);
@@ -192,11 +192,17 @@ public class BlueAllianceStone1Vuforia extends LinearOpMode {
             if (glyphPlacement == 3) {
                 telemetry.update();
                 closeLift();
-                moveLiftAmount(-.5,200);
-                encoderDrive(.5,4,4,4,4,4);
-                encoderDrive(.5,-3.35,3.35,-3.35,3.35,10);
-                encoderDrive(.5,7,7,7,7,4);
-                encoderDrive(.5,-3.35,3.35,-3.35,3.35,10);
+                moveLiftAmount(1,500);
+                navxTurn(90);
+                encoderDrive(.5,-5.75,-5.75,-5.75,-5.75,4);
+                sleep(1000);
+                navxTurn(179);
+                encoderDrive(.5,-1,-1,-1,-1,4);
+                openLift();
+                encoderDrive(.5,.5,.5,.5,.5,4);
+                closeLift();
+                moveLiftAmount(-1,500);
+                encoderDrive(.5,-.75,-.75,-.75,-.75,4);
             }
             telemetry.addData("Path", "Complete");
             telemetry.update();
