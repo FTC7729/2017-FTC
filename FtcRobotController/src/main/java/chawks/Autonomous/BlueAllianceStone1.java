bd69d98a2fb5aa48000cd83f142adac2d1f01a78
Kara Handy <karahandy@Karas-MacBook.local>
2018-02-20 17:38:35 -0500

    changed encoder value in Autonomous tonew wheels

--- FtcRobotController/src/main/java/chawks/Autonomous/BlueAllianceStone1.java
+++ FtcRobotController/src/main/java/chawks/Autonomous/BlueAllianceStone1.java
@@ -32,8 +32,6 @@ public class BlueAllianceStone1 extends LinearOpMode {
     static final double     DRIVE_GEAR_REDUCTION    = 1.5 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 3.93701 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-
-
             (WHEEL_DIAMETER_INCHES * Math.PI);
 
 
